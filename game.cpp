3d1d6f2554746dec9af6cd32f1b485f78e82d2dc
avisekk-helloworld <abh898ref@gmail.com>
2021-05-23 15:11:27 +0545

    made increaseScore,decreaseLives, other functionality

--- game.cpp
+++ game.cpp
@@ -74,15 +74,21 @@ public:
     // decreases the lives or ends the game.
     void decreaseLives()
     {
-        if (getLives())
+        lives -= 1;
+    }
+    bool shouldGameBeRunning()
+    {
+        if (getLives() + 1) // +1 done because for lives=0 also the if block should run.
         {
-            lives -= 1;
-        }else{
-            cout << "Game over...."<< endl;
-            cout << "Your score= " << score << endl;
+            return true;
+        }
+        else
+        {
+            cout << "Game over...."<<endl;
+            cout << "Your Score= "<< score<<endl;
+            return false;
         }
     }
-    void display();
     // overloaded functions
     friend istream &operator>>(istream &cin, Player &p);
 };
@@ -92,54 +98,77 @@ istream &operator>>(istream &cin, Player &p)
     cin >> p.name;
     return cin;
 }
-void Player::display()
-{
-    cout << "I am player. My name is: " << name << "\nScore = " << score << "\nLives= " << lives << endl;
-}
+
 class Game
 {
-    Player p;
+
     Question *currentQuestion;
     static Question *allQuestions;
 
 public:
+    // FIXME: Should i make the following data member public??
+    // the problem will occur if outside world changes the player object to some another player object
+    // but the other things like changing data of the same player object cant occur due to private 
+    // variables of the player object.
+    Player p;
     Game(Player &player) : p(player)
     {
         cout << "Game is being loaded....." << endl;
         createQuestions();
         currentQuestion = allQuestions;
     }
-    ~Game(){
+    ~Game()
+    {
         delete[] allQuestions;
     }
-    void nextQuestion(){
-        cout << currentQuestion->getQuestion() <<endl;
-        currentQuestion = allQuestions+1;
+    void nextQuestion()
+    {
+        cout << currentQuestion->getQuestion() << endl;
+    }
+    void checkAnswer(){
+        string answer;
+        cin >> answer;
+        if(answer==currentQuestion->getAnswer()){
+            currentQuestion = allQuestions + 1;
+            p.increaseScore();
+            cout << "Game Status: "<< endl;
+            cout << "Score: "<<p.getScore()<<endl;
+            cout << "Lives: "<<p.getLives()<<endl;
+        }else{
+            p.decreaseLives();
+            cout << "Game Status: "<< endl;
+            cout << "Score: "<<p.getScore()<<endl;
+            cout << "Lives: "<<p.getLives()<<endl;
+        }
     }
     static void createQuestions();
 };
-Question* Game::allQuestions = NULL;
+Question *Game::allQuestions = NULL;
 void Game::createQuestions()
+{
+    // local variable
+    int numOfQuestionsToBeCreated = Question::getMaxCount();
+    // member variable ( //TODO: allocating dynamically so we need destructor )
+    allQuestions = new Question[numOfQuestionsToBeCreated];
+
+    // FIXME: try to initialize the following array using numOfQuestionsToBeCreated variable.
+    Question ques[5] = {Question("What is the Capital City of Bhutan?", "thimpu"), Question("What is the Capital City of Australia?", "canberra"), Question("What is the Capital City of Germany?", "berlin"), Question("What is the Capital City of England?", "london"), Question("What is the Capital City of Ireland?", "dublin")};
+
+    for (int i = 0; i < numOfQuestionsToBeCreated; i++)
     {
-        // local variable
-        int numOfQuestionsToBeCreated = Question::getMaxCount();
-        // member variable ( //TODO: allocating dynamically so we need destructor )
-        allQuestions = new Question[numOfQuestionsToBeCreated];
-
-        // FIXME: try to initialize the following array using numOfQuestionsToBeCreated variable.
-        Question ques[5] = {Question("What is the Capital City of Bhutan?", "thimpu"), Question("What is the Capital City of Australia?", "canberra"), Question("What is the Capital City of Germany?", "berlin"), Question("What is the Capital City of England?", "london"), Question("What is the Capital City of Ireland?", "dublin")};
-
-        for (int i = 0; i < numOfQuestionsToBeCreated; i++)
-        {
-            *(allQuestions + i) = ques[i];
-        }
+        *(allQuestions + i) = ques[i];
     }
+}
 int main()
 {
     cout << "=======WELCOME TO THE QUIZ GAME=======" << endl;
     Player p1;
     cin >> p1;
     Game game(p1);
-    game.nextQuestion();
+    while (game.p.shouldGameBeRunning()){
+        game.nextQuestion();
+        game.checkAnswer();
+    }
+
     return 0;
 }
